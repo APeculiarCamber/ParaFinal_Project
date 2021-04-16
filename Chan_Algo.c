ab2a004ca079238140cded85b13191d4da51c6cf
lucasfs2020 <44509169+lucasfs2020@users.noreply.github.com>
2021-04-15 23:45:34 -0400

    Graham Algorithm
    continued working on graham scan

--- Chan_Algo.c
+++ Chan_Algo.c
@@ -11,6 +11,22 @@ struct Point{
 	float y;
 };
 
+struct Tuple{
+	struct Point p;
+	float angle;
+};
+
+struct Polar_vector{
+	struct Tuple * polar_list;
+	int size;
+};
+
+struct Point_stack{
+	struct Point* stack
+};
+
+typedef struct Tuple Tuple;
+typedef struct Polar_vector Polar_l;
 typedef struct Point Point;
 
 void readInData(char* fName, Point * points, int myrank, size_t stride, size_t numPoints){
@@ -60,7 +76,100 @@ float return_polar_angle(Point p1, Point p2){
 	return atan2(d_y,d_x);
 }
 
-void Perform_Graham(Point* point_set){
+Point bottom_left(Point * set_points, int num_points){
+	float min_y = 1000000;
+	Point min_point;
+	int i;
+	for (i = 0; i < num_points; i++){
+		if (set_points[i].y < min_y){
+			printf("min y: %f, min x: %f\n", min_point.x, min_point.y);
+			min_y = set_points[i].y;
+			min_point = set_points[i];
+		}
+		else if(set_points[i].x < min_point.x){
+			min_point = set_points[i];
+		}
+	}
+	return min_point;
+}
+
+Polar_l polar_points(Point * set_points, int num_points, Point min_point){
+	int i;
+	Polar_l point_list;
+	point_list.polar_list = calloc(num_points-1, sizeof(Tuple));
+	point_list.size = num_points-1;
+	int count = 0;
+	for (i = 0; i < num_points; i++){
+		if (set_points[i].y != min_point.y && set_points[i].x != min_point.x){
+			Point temp = set_points[i];
+			float temp_polar = return_polar_angle(temp, min_point);
+			Tuple temp_tuple;
+			temp_tuple.p = temp;
+			temp_tuple.angle = temp_polar;
+			point_list.polar_list[count] = temp_tuple;
+			count = count+1;
+		}
+	}
+	return point_list;
+}
+
+void* add_to_polar_l(Polar_l* c_list){
+	c_list->size = c_list->size*2;
+	c_list->polar_list = (realloc(c_list->polar_list, c_list->size*sizeof(Tuple)));
+}
+
+int item_exists(Polar_l exist_list, float angle){
+	int i;
+	for (i = 0; i < exist_list.size; i++){
+		if (angle == exist_list[i].angle){
+			return i;
+		}
+	}
+	return -1;
+}
+
+int distance_compare(Point p1, Point p2, min_point){
+	float d1 = sqrt((p1.x-min_point.x)**2+(p1.y-min_point.y)**2)
+	float d2 = sqrt((p2.x-min_point.x)**2+(p2.y-min_point.y)**2)
+
+	return d1-d2
+}
+
+Polar_l remove_repeat(Polar_l current_list, Point min_point){
+	Polar_l without_repeat;
+	int current_size = 8;
+
+	Polar_l without_repeat.polar_list = calloc(current_size, sizeof(Tuple));
+
+	int count = 0;
+	int i;
+	for (i = 0; i < current_list.size; i++){
+		Tuple current = current_list.polar_list[i];
+		int index = item_exists(current_size,angle);
+		if (index == -1){
+			count += 1;
+			if (count == current_size){
+				add_to_polar_l(&without_repeat);
+			}
+			without_repeat.polar_list[count] = current;
+		}
+		else{
+			if (distance_compare(current.p, without_repeat[index].p, min_point) < 0){
+				without_repeat[index] = current;
+			}
+		}
+	}
+	return without_repeat;
+}
+
+int compare (const void *a, const void * b){
+	Tuple *tupleA = (Tuple *a);
+	Tuple *tupleB = (Tuple *b);
+
+	return (tupleA->angle - tupleB->angle);
+}
+
+Point* Perform_Graham(Point* point_set){
 	/*
 	have list of poitns
 	create empty stack
@@ -74,24 +183,10 @@ void Perform_Graham(Point* point_set){
 	once have set, repeat process and find actual outer hull
 
 	*/
+	Point* hull;
 
+	
 
-}
-
-Point bottom_left(Point * set_points, int num_points){
-	float min_y = 1000000;
-	Point min_point
-	int i;
-	for (i = 0; i < num_points; i++){
-		printf("%f\n", (set_points[i].x));
-		if (set_points[i].y < min_y){
-			min_point = set_points[i]
-		}
-		else if(set_points[i].x < min_point.x){
-			min_point = set_points[i]
-		}
-	}
-	return min_point
 }
 
 int main(int argc, char*argv[]){
@@ -120,7 +215,25 @@ int main(int argc, char*argv[]){
 	Point * points = malloc(numPoints * sizeof(Point));
 	readInData(argv[2], points, myrank, (totalPoints/numranks)*sizeof(Point), numPoints);
 
-	bottom_left(points, numPoints);
+	Point min_point = bottom_left(points, numPoints);
+
+	Polar_l p_points = polar_points(points, numPoints, min_point);
+
+	qsort(p_points, numPoints-1, Tuple, compare);
+
+	Polar_l final_polar_set = remove_repeat(p_points, min_point);
+
+	if (final_polar_set.size < 3){
+		return 0
+	}
+
+	Point* final_point_set = calloc(final_polar_set.size, sizeof(Point));
+	int i;
+	for (i = 0; i < final_set.size; i++){
+		final_point_set[i] = final_polar_set.polar_list[i];
+	}
+
+	Point* possible_hull = Perform_Graham(final_point_set);
 
 	return 0;
 }
