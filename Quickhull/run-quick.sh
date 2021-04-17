11f5ab70c8cabb49cd0680c022699d0e7e606a2d
Chris Vanderloo <chris.vanderloo@yahoo.com>
2021-04-17 19:02:54 -0400

    changed script

--- Quickhull/run-quick.sh
+++ Quickhull/run-quick.sh
@@ -1,3 +1,3 @@
 module load spectrum-mpi xl_r
 mpicc Quickhull.c -o quickhull
-sbatch -N $1 --ntasks-per-node=$2 --gres=gpu:1 -t 10 ./quickhull.sh
+sbatch -N $1 --ntasks-per-node=$2 --gres=gpu:1 -t 10 ./slurm_quick.sh
