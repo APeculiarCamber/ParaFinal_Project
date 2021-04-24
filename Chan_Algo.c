9b429d128a874a9fc5a3cf620a323a028efdb55a
lucasfs2020 <44509169+lucasfs2020@users.noreply.github.com>
2021-04-24 14:58:56 -0400

    Updates to Chan's algo
    Currently working on data and getting chan's up and running

--- Chan_Algo.c
+++ Chan_Algo.c
@@ -19,24 +19,42 @@ struct Tuple{
 struct Polar_vector{
 	struct Tuple * polar_list;
 	int size;
+	int count;
 };
 
 struct Point_stack{
 	struct Point* stack;
 	int size;
+	int count;
 };
 
 typedef struct Tuple Tuple;
 typedef struct Polar_vector Polar_l;
 typedef struct Point_stack Polar_s;
 typedef struct Point Point;
+typedef unsigned long long ticks;
+
+static __inline__ ticks getticks(void){
+	unsigned int tb1, tbu0, tbu1;
+	do{
+		__asm__ __volatile__ ("mftbu %0" : "=r"(tbu0));
+		__asm__ __volatile__ ("mftbu %0" : "=r"(tb1));
+		__asm__ __volatile__ ("mftbu %0" : "=r"(tbu1));
+	}while(tbu0 != tbu1);
+	return ((((unsigned long long)tbu0) << 32) | tb1);
+}
 
 void readInData(char* fName, Point * points, int myrank, size_t stride, size_t numPoints){
 	MPI_File mpiFile;
 	MPI_Status stat;
+	//after open, have offset be based upon rank
+	printf("starting point is %zu at %d\n", myrank*numPoints*sizeof(Point), myrank);
 	int rc = MPI_File_open(MPI_COMM_WORLD, fName, MPI_MODE_RDONLY, MPI_INFO_NULL, &mpiFile);
-	rc = MPI_File_read_at(mpiFile, (myrank*stride), points, numPoints, MPI_FLOAT, &stat);
+	printf("rc is %d\n", rc);
+	rc = MPI_File_read_at(mpiFile, myrank*numPoints*sizeof(Point), points, 2*numPoints, MPI_FLOAT, &stat);
+	printf("rc is %d\n", rc);
 	rc = MPI_File_close(&mpiFile);
+	printf("rc is %d\n", rc);
 }
 
 MPI_Datatype MPI_POINT;
@@ -94,22 +112,22 @@ Point bottom_left(Point * set_points, int num_points){
 	int i;
 	for (i = 0; i < num_points; i++){
 		if (set_points[i].y < min_y){
-			printf("min y: %f, min x: %f\n", min_point.x, min_point.y);
+			//printf("min y: %f, min x: %f\n", min_point.y, min_point.x);
 			min_y = set_points[i].y;
 			min_point = set_points[i];
 		}
-		else if(set_points[i].x < min_point.x){
-			min_point = set_points[i];
+		else if(set_points[i].y == min_y){
+			if (set_points[i].x < min_point.x){
+				min_point = set_points[i];
+			}
 		}
 	}
 	return min_point;
 }
 
-Polar_l polar_points(Point * set_points, int num_points, Point min_point){
+Tuple* polar_points(Point * set_points, int num_points, Point min_point){
 	int i;
-	Polar_l point_list;
-	point_list.polar_list = calloc(num_points-1, sizeof(Tuple));
-	point_list.size = num_points-1;
+	Tuple* point_list = calloc(num_points-1, sizeof(Tuple));
 	int count = 0;
 	for (i = 0; i < num_points; i++){
 		if (set_points[i].y != min_point.y && set_points[i].x != min_point.x){
@@ -118,16 +136,11 @@ Polar_l polar_points(Point * set_points, int num_points, Point min_point){
 			Tuple temp_tuple;
 			temp_tuple.p = temp;
 			temp_tuple.angle = temp_polar;
-			point_list.polar_list[count] = temp_tuple;
+			point_list[count] = temp_tuple;
 			count = count+1;
 		}
 	}
 	return point_list;
-}
-
-void* add_to_polar_l(Polar_l* c_list){
-	c_list->size = c_list->size*2;
-	c_list->polar_list = (realloc(c_list->polar_list, c_list->size*sizeof(Tuple)));
 }
 
 int item_exists(Polar_l exist_list, float angle){
@@ -147,21 +160,26 @@ int distance_compare(Point p1, Point p2, Point min_point){
 	return d1-d2;
 }
 
-Polar_l remove_repeat(Polar_l current_list, Point min_point){
+Polar_l remove_repeat(Tuple* current_list, Point min_point, int numPoints){
 	Polar_l without_repeat;
 	int current_size = 8;
 
 	without_repeat.polar_list = calloc(current_size, sizeof(Tuple));
+	without_repeat.size = current_size;
+	without_repeat.count = 0;
 
 	int count = 0;
 	int i;
-	for (i = 0; i < current_list.size; i++){
-		Tuple current = current_list.polar_list[i];
+	for (i = 0; i < numPoints-1; i++){
+		Tuple current = current_list[i];
 		int index = item_exists(without_repeat, current.angle);
 		if (index == -1){
 			count += 1;
+			without_repeat.count = without_repeat.count+1;
 			if (count == current_size){
-				add_to_polar_l(&without_repeat);
+				without_repeat.size = without_repeat.size*2;
+				current_size = current_size*2;
+				without_repeat.polar_list = realloc(without_repeat.polar_list, without_repeat.size*sizeof(Tuple));
 			}
 			without_repeat.polar_list[count] = current;
 		}
@@ -279,15 +297,7 @@ Point* perform_chan(Point* hull_set, Point min_point, int m){
 }
 
 int main(int argc, char*argv[]){
-	/*
-	In main, first grab the data
 
-	split into squares
-
-	perform Graham on different hulls
-
-	then find outer 
-	*/
 	int myrank, numranks;
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
@@ -304,39 +314,32 @@ int main(int argc, char*argv[]){
 	Point * points = malloc(numPoints * sizeof(Point));
 	readInData(argv[2], points, myrank, (totalPoints/numranks)*sizeof(Point), numPoints);
 
-	Point** point_set;
-
-	int num_sets = 4;
-
-	createSamples(points, num_sets, &point_set, numPoints);
-
 	int i;
 
-	int size_per_set = numPoints/num_sets;
-
-	for (i = 0; i < num_sets; i++){
-		int j;
-		for (j = 0; j < size_per_set; j++){
-			Point curr_point = point_set[i][j];
-			printf("point set %d, x is %f, y is %f\n", i, curr_point.x, curr_point.y);
-		}
+	for (i = 0; i < numPoints; i++){
+		printf("point is %f, %f, rank is %d\n", points[i].x, points[i].y, myrank);
 	}
 
 	Point min_point = bottom_left(points, numPoints);
 
-	Polar_l p_points = polar_points(points, numPoints, min_point);
+	Tuple* p_points = polar_points(points, numPoints, min_point);
 
-	qsort(&p_points.polar_list, numPoints-1, sizeof(Tuple), compare);
+	qsort(p_points, (size_t)numPoints-1, sizeof(Tuple), compare);
 
-	Polar_l final_polar_set = remove_repeat(p_points, min_point);
+	Polar_l final_polar_set = remove_repeat(p_points, min_point, numPoints);
 
+	printf("size of final set is %d\n", final_polar_set.size);
+	
 	if (final_polar_set.size < 3){
 		return 0;
 	}
 
+	for (i = 0; i < final_polar_set.count; i++){
+		printf("%d final point is %f, %f, rank is %d\n", i, final_polar_set.polar_list[i].p.x, final_polar_set.polar_list[i].p.y, myrank);
+	}
+
 	Point* final_point_set = calloc(final_polar_set.size, sizeof(Point));
-	int i;
-	for (i = 0; i < final_polar_set.size; i++){
+	for (i = 0; i < final_polar_set.count; i++){
 		final_point_set[i] = final_polar_set.polar_list[i].p;
 	}
 
@@ -345,8 +348,8 @@ int main(int argc, char*argv[]){
 	Point* set = calloc(hull.size, sizeof(Point));
 
 	for (i = 0; i < hull.size; i++){
+		printf("outer hull is x:%f, y:%f, rank is %d\n", hull.stack[i].x, hull.stack[i].y, myrank);
 		set[i] = hull.stack[i];
 	}
-
 	return 0;
 }
